1f1bf749eaf9248603910755188554957980b370
vasilieva-am <118099365+vasilieva-am@users.noreply.github.com>
2022-12-02 23:53:07 +0300

    Update task_3.py

--- Лабораторная №4/task_3.py
+++ Лабораторная №4/task_3.py
@@ -4,11 +4,9 @@ def delete(list_, index=None):
     else:
         _ = index+1
         end_list = list_[:index] + list_[_:]
-
     return end_list
 
-
-print(delete([0, 1, 2], index=0))  # [0, 1]
+print(delete([0, 1, 2], index=0))  # [1, 2]
 print(delete([0, 1, 2], index=1))  # [0, 2]
 print(delete([0, 1, 2]))  # [0, 1]
 
