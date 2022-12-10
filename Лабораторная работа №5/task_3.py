0a0f30b391661f40a03fae9c9c4f0743d5a8b309
vasilieva-am <118099365+vasilieva-am@users.noreply.github.com>
2022-12-11 00:02:01 +0300

    Update task_3.py

--- Лабораторная работа №5/task_3.py
+++ Лабораторная работа №5/task_3.py
@@ -1,11 +1,12 @@
-import random
+from random import randint
 def get_unique_list_numbers() -> list[int]:
+    list_numbers = []
     list_size = 15
-    l = []
-    while len(l) < list_size:
-        list_ = list(set(random.randint(-10, 10) for num in range(list_size)))
-        break
-    return list_
+    while len(list_numbers) < list_size:
+        number = randint(-10, 10)
+        if number not in list_numbers:
+            list_numbers.append(number)
+    return list_numbers
 
 list_unique_numbers = get_unique_list_numbers()
 print(list_unique_numbers)
