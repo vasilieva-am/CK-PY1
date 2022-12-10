07b53b08a49164de7a855ff00f791661c0f59086
vasilieva-am <118099365+vasilieva-am@users.noreply.github.com>
2022-12-10 23:37:58 +0300

    Update task_1.py

--- Лабораторная работа №5/task_1.py
+++ Лабораторная работа №5/task_1.py
@@ -1,4 +1,3 @@
-# TODO решить с помощью list comprehension и распечатать его
 from pprint import pprint
 list_dictionary = [{'bin': bin(num), 'dec': num, 'hex': hex(num), 'oct': oct(num)} for num in range(0, 16)]
 
