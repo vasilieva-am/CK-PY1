fbc1c7fb29c26b0c3da25c1af957963d5f2d7bfa
vasilieva-am <118099365+vasilieva-am@users.noreply.github.com>
2022-12-10 23:38:39 +0300

    Update task_3.py

--- Лабораторная работа №5/task_3.py
+++ Лабораторная работа №5/task_3.py
@@ -10,3 +10,4 @@ def get_unique_list_numbers() -> list[int]:
 list_unique_numbers = get_unique_list_numbers()
 print(list_unique_numbers)
 print(len(list_unique_numbers) == len(set(list_unique_numbers)))
+
