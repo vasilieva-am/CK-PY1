fe3fa247741dc3c532d2c4f3f187ec2be951b533
vasilieva-am <118099365+vasilieva-am@users.noreply.github.com>
2022-12-25 22:34:19 +0300

    Update task_4.py

--- Лабораторная работа №6/task_4.py
+++ Лабораторная работа №6/task_4.py
@@ -5,7 +5,7 @@ INPUT_FILE = "input1.csv"
 def csv_to_list_dict(INPUT_FILE, delimiter=',', new_line='\n'):
     with open(INPUT_FILE, "r") as f:
         heads = f.readline()[:-1].split(delimiter)
-        values = [i[:-1].split(delimiter) for i in f.readlines()]
+        values = [i[:-1].split(delimiter) for i in f]
 
     return [dict(zip(heads, values[j])) for j in range(len(values))]
 
